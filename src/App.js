f30779a95222d526655bf1dacc91c6d6de0e6727
ozgursayin <ozgursayin@gmail.com>
2021-04-26 17:07:00 +0300

    Add connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NavBar from "./components/NavBar";
 
-function App() {
+const App = () => {
+  const [songs, setSongs] = useState([]);
+  const [genres, setGenres] = useState([]);
+
+  const BASE_URL =
+    "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/main/data/dataset.json";
+
+  const fetchData = async () => {
+    const result = await axios
+      .get(BASE_URL)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+
+    setSongs(result?.videos);
+    setGenres(result?.genres);
+  };
+
+  console.log("songs", songs);
+  console.log("genres", genres);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div>
+        {genres.map((g) => (
+          <div key={g.id}>{g.name}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import logo from "./xitelogo.png";
+
+const NavBar = () => {
+  return (
+    <div className="navBarTop">
+      <img className="logo" src={logo} alt="" />
+    </div>
+  );
+};
+
+export default NavBar;
